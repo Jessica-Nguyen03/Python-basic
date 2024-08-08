4e67f647224f9858bae2ac8e1a33343b707475ff
Nguyen Tham <nguyenthihongtham190503@gmail.com>
2024-08-08 09:37:30 +0700

    nlpk update

--- work-with-textdata/nlpk.py
+++ work-with-textdata/nlpk.py
@@ -1,12 +1,25 @@
-from newspaper import Article
+# from newspaper import Article
 
-article = Article('https://vnexpress.net/ky-su-phan-mem-ai-dau-tien-tren-the-gioi-4722040.html')
+# article = Article('https://vnexpress.net/ky-su-phan-mem-ai-dau-tien-tren-the-gioi-4722040.html')
 
-#download
-article.download()
-article.parse()
+# #download
+# article.download()
+# article.parse()
 
-#in bai bao
+# #in bai bao
 
-print(article.text)
-print(article.images)
+# print(article.text)
+# print(article.images)
+
+
+import nltk
+from nltk . tokenize import word_tokenize
+
+nltk . download ('punkt')
+
+data = "Tôi thích học AI và Toán"
+# Bước 1: Tokenization data
+tokenization = word_tokenize ( data )
+# Bước 2: Gọi thư viện Pos tagging
+result = nltk . pos_tag ( tokenization )
+print ( result )
